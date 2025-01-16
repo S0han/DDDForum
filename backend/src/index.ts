82398c3ecc331f0a6017db34c571e5ac98c62db4
S0han <kane.pereira18@gmail.com>
2025-01-15 23:40:24 -0500

    [update]: create post operation to write to DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient as prisma } from '@prisma/client';
 
 const app = express();
 const PORT = 3000;
@@ -12,4 +13,23 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
+});
+
+app.post('/users/new', async (req: Request, res: Response) => {
+    const { email, username, firstName, lastName, password } = req.body;
+
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                email,
+                username,
+                firstName,
+                lastName,
+                password,
+            }
+        });
+        res.status(201).json(newUser);
+    } catch (e) {
+        res.status(400)
+    }
 });
