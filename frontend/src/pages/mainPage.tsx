03b24cf02f4a5c7ad118aeb2f0b6f631aaae32ae
S0han <kane.pereira18@gmail.com>
2025-01-21 01:03:42 -0500

    [update]: add other components containing dummy data

--- frontend/src/pages/mainPage.tsx
+++ frontend/src/pages/mainPage.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Layout } from '../components/layout';
+import { Layout } from "../components/layout";
+import { PostsList } from "../components/postsList.tsx";
+import { PostsViewSwitcher } from "../components/postsViewSwitcher";
+import { api } from "../api";
 
 export const MainPage = () => {
+  const posts = [{ ... }];
 
   return (
     <Layout>
-      
+      <PostsViewSwitcher />
+      <PostsList
+        posts={posts}
+      />
     </Layout>
   );
 };
