acfba0b3399aaa89e7e88bcc86f2a84917c530d8
S0han <kane.pereira18@gmail.com>
2025-01-20 23:39:02 -0500

    conn-frnt-bk

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the DDD Forum</h1>
+      <p>This is the home page of the forum.</p>
+    </div>
+  );
+}
+
+export default App;
